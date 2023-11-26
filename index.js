203d5a41a87187c84fad3d75816f629b5e79fe1b
raselsharif <raselsharif9@gmail.com>
2023-11-27 02:42:23 +0600

    added asset get, asset delet api

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // middlewares
 app.use(cors());
@@ -29,11 +29,39 @@ async function run() {
     // collections
     const database = client.db("assets-management");
     const userCollection = database.collection("users");
+    const assetCollection = database.collection("assets");
 
     // user api's
+    app.get("/users", async (req, res) => {
+      const users = await userCollection.find().toArray();
+      res.send(users);
+    });
+    // get single user
+    app.get("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const result = await userCollection.findOne(filter);
+      res.send(result);
+    });
     app.post("/user", async (req, res) => {
       const user = req.body;
       const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+    // admin api
+    app.get("/assets", async (req, res) => {
+      const assets = await assetCollection.find().toArray();
+      res.send(assets);
+    });
+    app.post("/add-asset", async (req, res) => {
+      const asset = req.body;
+      const result = await assetCollection.insertOne(asset);
+      res.send(result);
+    });
+    app.delete("/asset-delete/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await assetCollection.deleteOne(filter);
       res.send(result);
     });
     // Send a ping to confirm a successful connection
